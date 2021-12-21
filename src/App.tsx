6d5bf9e3f5c30df38eca1cf6877604523cd87d8a
JYS <runnway@naver.com>
2021-12-22 01:22:14 +0900

    prepare dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+// import { hourSelector, minuteState } from "./atoms";
 
 const App = () => {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
+  const onDragEnd = () => {};
   return (
-    <div>
-      <input
-        type="number"
-        placeholder="Minute"
-        value={minutes}
-        onChange={onMinuteChange}
-      />
-      <input
-        type="number"
-        placeholder="Hours"
-        onChange={onHourChange}
-        value={hours}
-      />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>ONE</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>TWO</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,16 +1,16 @@
 import { atom, selector } from "recoil";
 
-export const minuteState = atom({ key: "minutes", default: 0 });
+// export const minuteState = atom({ key: "minutes", default: 0 });
 
-export const hourSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minute = get(minuteState);
+// export const hourSelector = selector<number>({
+//   key: "hours",
+//   get: ({ get }) => {
+//     const minute = get(minuteState);
 
-    return Math.floor(minute / 60);
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    set(minuteState, minutes);
-  },
-});
+//     return Math.floor(minute / 60);
+//   },
+//   set: ({ set }, newValue) => {
+//     const minutes = Number(newValue) * 60;
+//     set(minuteState, minutes);
+//   },
+// });
